fb748bce958af974fc7c5f3e9cf2e20d9e443e7f
Maria Brock <brockm@ornl.giv>
2024-06-05 09:59:05 -0400

    login and register both work

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -5,6 +5,9 @@ import { MatButton } from "@angular/material/button";
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms";
 import { MatInput } from "@angular/material/input";
 import { Router, RouterLink } from "@angular/router";
+import { HttpClient } from "@angular/common/http";
+import { AuthService } from "../auth.service";
+import { User } from "../../interfaces/user";
 
 @Component({
   selector: 'app-login',
@@ -27,22 +30,30 @@ import { Router, RouterLink } from "@angular/router";
 export class LoginComponent {
 
   private router = inject(Router);
+  private http = inject(HttpClient);
+  private authService = inject(AuthService);
 
-  public  loginGroup: FormGroup;
+  loginGroup = new FormGroup<any>({
+    email: new FormControl('', [Validators.required]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+      Validators.maxLength(20)
+    ]),
+  });
 
-  constructor() {
-    this.loginGroup = new FormGroup<any>({
-      username: new FormControl('', [Validators.required]),
-      password: new FormControl('', [
-        Validators.required,
-        Validators.minLength(6),
-        Validators.maxLength(20)
-      ]),
-    });
-  }
+  constructor() {}
 
   public login() {
-    console.log("You've logged in.")
+    console.log(this.loginGroup.getRawValue())
+    this.http.post<{user: User}>(
+      'https://api.realworld.io/api/users/login',
+      {user: this.loginGroup.getRawValue()})
+      .subscribe((response) => {
+        console.log('response', response);
+        localStorage.setItem('token', response.user.token);
+        this.authService.currentUserSignal.set(response.user);
+      })
     this.router.navigate(['/landing']).then()
   }
 }
